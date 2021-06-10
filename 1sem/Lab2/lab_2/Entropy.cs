24e732827a6a1c91fba877c890a851ada98688d1
vrnchll <veronichka127@mail.ru>
2021-06-10 15:49:22 +0300

    add labs for 1 semester

---
+++ 1sem/Lab2/lab_2/Entropy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_2
+{
+    public static class Entropy
+    {
+        public static double CalculateShannonEntropy(string s)
+        {
+            var map = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (!map.ContainsKey(c))
+                {
+                    map.Add(c, 1);
+                }
+                else
+                {
+                    map[c] += 1;
+                }
+            }
+
+            int len = s.Length;
+            double result = 0D;
+            //var frequency_map = new SortedDictionary<char, double>();
+            foreach (var item in map)
+            {
+                var frequency = (double)item.Value / len;
+                //frequency_map.Add(item.Key, frequency);
+                result -= frequency * Math.Log(frequency, 2);
+            }
+
+            //foreach (var item in frequency_map)
+            //{
+            //    Console.WriteLine($"{item.Key} - {item.Value}");
+            //}
+            //Console.WriteLine();
+
+            return result;
+        }
+
+        public static double GetAmountOfInformation(string s, double entropy)
+        {
+            double result = s.Length * entropy;
+            return result;
+        }
+
+        public static double GetAmountOfInformationWithMistake(string s, double entropy, double p)
+        {
+            double Hyx = -p * Math.Log(p, 2) - (1 - p) * Math.Log(1 - p, 2);
+            double result = (entropy - Hyx) * s.Length;
+            return result;
+        }
+
+        public static double GetAmountOfInformationWithMistake(string s, double p)
+        {
+            double Hyx = -p * Math.Log(p, 2) - (1 - p) * Math.Log(1 - p, 2);
+            double result = (1 - Hyx) * s.Length;
+            return result;
+        }
+    }
+}
---
+++ 1sem/Lab2/lab_2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string en_text = "";
+            string ru_text = "";
+
+            using (StreamReader reader = new StreamReader("en.txt"))
+            {
+                en_text = Regex.Replace(reader.ReadToEnd(), @"[^A-Za-z]", "").ToLower();
+            }
+            using (StreamReader reader = new StreamReader("ru.txt"))
+            {
+                ru_text = Regex.Replace(reader.ReadToEnd(), @"[^А-Яа-я]", "").ToLower();
+            }
+
+            byte[] bytes = Encoding.ASCII.GetBytes(en_text);
+            var bits = new BitArray(bytes);
+            StringBuilder ASCII = new StringBuilder();
+            foreach (bool b in bits)
+                ASCII.Append(b ? 1 : 0);
+
+            using (StreamWriter sw = new StreamWriter("bin.txt", false, Encoding.ASCII))
+            { sw.Write(ASCII); }
+
+            double ru_entropy = Entropy.CalculateShannonEntropy(ru_text);
+            double en_entropy = Entropy.CalculateShannonEntropy(en_text);
+            double bin_entropy = Entropy.CalculateShannonEntropy(ASCII.ToString());
+
+            Console.WriteLine($"Entropy of english alphabet = {en_entropy}");
+            Console.WriteLine($"Entropy of russian alphabet = {ru_entropy}");
+            Console.WriteLine($"Entropy of binary alphabet = {bin_entropy}");
+
+            string ru_name = "максимчиковаюлиясергеевна";
+           // ru_entropy = Entropy.CalculateShannonEntropy(ru_name);
+
+            Console.WriteLine($"\nAmount of information in ru_name = {Entropy.GetAmountOfInformation(ru_name, ru_entropy)}");
+
+            Console.WriteLine($"\nWith conditional error probability 0.1 = {Entropy.GetAmountOfInformationWithMistake(ru_name, ru_entropy, 0.1)}");
+            Console.WriteLine($"With conditional error probability 0.5 = {Entropy.GetAmountOfInformationWithMistake(ru_name, ru_entropy, 0.5)}");
+            Console.WriteLine($"With conditional error probability 1 = {Entropy.GetAmountOfInformation(ru_name, ru_entropy)}");
+
+            bytes = Encoding.ASCII.GetBytes(ru_name);
+            ASCII = new StringBuilder();
+            bits = new BitArray(bytes);
+            foreach (bool b in bits)
+                ASCII.Append(b ? 1 : 0);
+
+            //bin_entropy = Entropy.CalculateShannonEntropy(ASCII.ToString());
+
+            Console.WriteLine($"\nAmount of information in ASCII = {Entropy.GetAmountOfInformation(ASCII.ToString(), 1)}");
+
+            Console.WriteLine($"\nWith conditional error probability 0.1 = {Entropy.GetAmountOfInformationWithMistake(ASCII.ToString(), 0.1)}");
+            Console.WriteLine($"With conditional error probability 0.5 = {Entropy.GetAmountOfInformationWithMistake(ASCII.ToString(), 0.5)}");
+            Console.WriteLine($"With conditional error probability 1 = {Entropy.GetAmountOfInformation(ASCII.ToString(), 1)}");
+
+            Console.ReadKey();
+        }
+    }
+}
